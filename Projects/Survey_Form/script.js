3f9aca895aa258ba5afa51b512381500251d721b
Vidhi Jindal <vidhijin11@gmail.com>
2024-08-04 00:35:35 +0530

    modified files

--- Projects/Survey_Form/script.js
+++ Projects/Survey_Form/script.js
@@ -1,26 +1,35 @@
 let popup = document.getElementById('pop-up');
-function openPopUp(){
+
+function openPopUp() {
     popup.classList.add("open-pop");
 }
-function closePopUp(){
+
+function closePopUp() {
     popup.classList.remove("open-pop");
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+
+    if (!form.checkValidity()) {
+        event.stopPropagation();
+    } else {
+        openPopUp();
+        form.reset();
+    }
+
+    form.classList.add('was-validated');
 }
 
 (() => {
     'use strict'
-  
+
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
-  
+
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
+        form.addEventListener('submit', handleSubmit, false);
     })
-  })()
+})();
