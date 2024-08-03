cfd0f34208916d040a2fa8e00bf97f66ae70aa63
Vidhi Jindal <vidhijin11@gmail.com>
2024-08-03 23:50:21 +0530

    added functionality

---
+++ Projects/Survey_Form/script.js
@@ -0,0 +1,26 @@
+let popup = document.getElementById('pop-up');
+function openPopUp(){
+    popup.classList.add("open-pop");
+}
+function closePopUp(){
+    popup.classList.remove("open-pop");
+}
+
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
